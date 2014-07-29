e8745a301dba1cc9dbcbfd8ec3c6d2e103c3fa09
Graham Curl <gac.curl@gmail.com>
2014-07-29 09:32:25 +0100

    initial rails app for project 1 Tic Tac Toe

---
+++ tttgame/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tttgame::Application.config.secret_token = 'f8cb3924a098b700a28587c4ddb89967b1912413b77dbbc7c025664679f7516edb7e8eb3c7055cd393bf1207fae554b13ab22fa45cafe08b38f6cabc34d905d1'
