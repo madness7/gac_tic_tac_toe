4d5808591b96f0250350be96c089c24a06669a7b
Graham Curl <gac.curl@gmail.com>
2014-07-31 16:29:53 +0100

    working with leaderboard

--- tttgame/app/helpers/games_helper.rb
+++ tttgame/app/helpers/games_helper.rb
@@ -9,4 +9,18 @@ module GamesHelper
       return "<a href='/play/" + move.game.id.to_s + "?moveid=" + move.id.to_s + "'></a>" 
     end 
   end
+  def find_winner_name(winner_id)
+    u = User.find winner_id
+    "#{u.user_name}"  
+  end
+  def calculate_leaderboard
+    results = User.all.map do |user|
+      winning_games_count = Game.all.select do |game|
+        game.winner_id == user.id
+      end.count 
+      [winning_games_count, user.user_name]
+    end
+    results.sort
+  end
+
 end
